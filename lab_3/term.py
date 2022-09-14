00aa407f5432299c8bbede1dacb542884af4e029
szym-i <smilczarczyk@student.agh.edu.pl>
2022-09-14 22:27:02 +0200

    Term class methods

--- lab_3/term.py
+++ lab_3/term.py
@@ -5,8 +5,41 @@ class Term:
         self.duration = term_duration
 
     def __str__(self):
+        if self.minute < 10:
+            return f'{self.hour}:0{str(self.minute)} [{self.duration}]'
         return f'{self.hour}:{self.minute} [{self.duration}]'
 
+    def earlierThan(self, term):
+        end_hour = (self.hour + (self.duration+self.minute)//60)%24
+        end_minute = (self.minute + self.duration%60)%60
+        if end_hour < term.hour: 
+            return True
+        elif end_hour == term.hour:
+            if end_minute < term.minute:
+                return True
+        return False
+    
+    def laterThan(self, term):
+        end_hour = (term.hour + (term.minute+term.duration)//60)%24
+        end_minute = (term.minute + term.duration%60)%60
+        if self.hour > end_hour:
+            return True
+        elif self.hour == end_hour:
+            if self.minute > end_minute:
+                return True
+        return False
+
+    def equals(self, term):
+        if self.hour == term.hour and self.minute == term.minute and self.duration == term.duration:
+            return f'Terms {self} and {term} are equal'
+        else:
+            return f'Terms {self} and {term} are NOT equal'
+
 if __name__ == '__main__':
-    term = Term(10,11,90)
-    print(term.__str__())
+    term1 = Term(10,50,60)
+    term2 = Term(9,50,60)
+    print(term1.__str__())
+    print(term2.__str__())
+    print(term1.earlierThan(term2))
+    print(term1.laterThan(term2))
+    
