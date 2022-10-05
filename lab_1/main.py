6b52ed6570fc364f37e7e715298686daf8a3c088
szym-i <smilczarczyk@student.agh.edu.pl>
2022-10-05 13:17:53 +0200

    main.py edit

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,8 +1,22 @@
-import sys
+#import main
+import cmath
 
 def sum(arg1, arg2):
-    return arg1 + arg2
+    try:
+        arg1 = complex(arg1)
+    except:
+        raise ValueError
+        return arg2
+    try:
+        arg2 = complex(arg2)
+    except:
+        raise ValueError
+        return arg1
+    return arg1+arg2
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-print(sum(a,b))
+
+if __name__ == '__main__':
+    a = (9 + 1j)
+    b = 1
+    print(f"suma = {sum(a,b)}")
+    print(f"__name__ = {__name__}")
