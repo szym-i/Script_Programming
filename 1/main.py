34a25762ef8648c172266aefc6c036763671a1b1
szym-i <smilczarczyk@student.agh.edu.pl>
2022-10-05 17:06:42 +0200

    Fractions added

--- 1/main.py
+++ 1/main.py
@@ -1,22 +1,22 @@
 #import main
 import cmath
+from fractions import Fraction
 
 def sum(arg1, arg2):
     try:
         arg1 = complex(arg1)
     except:
-        raise ValueError
         return arg2
     try:
         arg2 = complex(arg2)
     except:
-        raise ValueError
         return arg1
     return arg1+arg2
-
+    
 
 if __name__ == '__main__':
-    a = (9 + 1j)
-    b = 1
+    a = 9 + 1j
+    b = Fraction(2/4)
+    print(b)
     print(f"suma = {sum(a,b)}")
     print(f"__name__ = {__name__}")
--- 1/test.py
+++ 1/test.py
@@ -1,23 +1,34 @@
-import main
+from main import sum, Fraction
 import unittest
 
 
 class Test_TestSum(unittest.TestCase):
     def test_sum_integer_integer(self):
-        self.assertEqual(main.sum(2, 2), 4)
+        self.assertEquals(sum(2, 2), 4)
 
     def test_sum_integer_float(self):
-        self.assertEqual(main.sum(2, 1.5), 3.5)
+        self.assertEquals(sum(2, 1.5), 3.5)
 
     def test_sum_integer_string(self):
-        self.assertEqual(main.sum(2, '2'), 4)
+        self.assertEquals(sum(2, '2'), 4)
 
     def test_sum_string_string(self):
-         self.assertEqual(main.sum('2.1', '2.0'), 4.1)
+         self.assertEquals(sum('2.1', '2.0'), 4.1)
 
     def test_sum_integer_wrong_number_in_string(self):
-         self.assertEqual(main.sum(2, 'Ala ma kota123'), 2)
+         self.assertEquals(sum(2, 'Ala ma kota123'), 2)
 
+    def test_sum_complex_integer(self):
+        self.assertEquals(sum(2+1j,2),4+1j)
+
+    def test_sum_complex_complex(self):
+        self.assertEquals(sum(4+2j,2+1j),6+3j)
+    
+    def test_fraction_complex(self):
+        self.assertEquals(sum(5+2j,Fraction(2/4)),5.5+2j)
+
+    def test_fraction_fraction(self):
+        self.assertEquals(sum(Fraction(11/33),Fraction(1/3)),2/3)
 
 if __name__ == '__main__':
     unittest.main()
