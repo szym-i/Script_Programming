16e2c55c73137a1e26e857d321ebac4fee2f76f0
szym-i <smilczarczyk@student.agh.edu.pl>
2022-12-21 13:58:32 +0100

    switch from method GET to POST

--- 10/lab/server.js
+++ 10/lab/server.js
@@ -19,48 +19,83 @@ class Book{
     print(){
         return `${this.title} ${String(this.authors)} : ${this.n}`
     }
-    borrow(){
+    borrow(user){
         if (this.n > 0){
             this.n-=1;
+            user.borrowed.push(this.name);
             return 'The book has been borrowed'
         }
         else{
             return "Not enough books"
         }
     }
-    return(){
-        this.n+=1;
-        return 'The book has been returned'
+    return(user){
+        if (user.borrowed.includes(this.name)){
+            this.n+=1;
+            return 'The book has been returned'
+        }
+        else
+            return 'You cannot return this book'
     }
+}
+
+function readHTML(arg){
+    if (fs.existsSync(arg) === true){
+        if (fs.lstatSync(arg).isFile()) {
+            return(`${fs.readFileSync(arg, 'utf8')}`);
+        }
+    }
+}
+
+function handle(m){
+    console.log(m);
+    arr = m.split('+');
+    console.log(arr);
+    var op = arr.at(0); 
+    var title = arr.slice(1,-1).join(' ');
+    var author = arr.at(-1);
+    console.log(`op = ${op}`);
+    console.log(`title = ${title}`);
+    console.log(`author = ${author}`);
 }
 
 function requestListener(request, response) {
 	var url = new URL(request.url, `http://${request.headers.host}`);
-	if (url.pathname == '/submit') {
-		if (request.method == 'GET') {
-            let p = url.searchParams.get('path');
-			process(response, p);
-        }
-        else {
-            response.writeHead(405, { "Content-Type": "text/plain; charset=utf-8" });
-			response.write(`This application does not support the ${request.method} method`);
-            response.end();
-        }
-	}
+    if (request.method == 'POST') {
+        var body = ''
+        request.on('data', function(data) {
+             body += data;
+        })
+        request.on('end', function() {
+            body = body.slice(5);
+            debug('Received body: ' + body);
+            handle(body);
+            response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+            response.write(body);
+            response.end('\npost received');
+        })
+    }
+	// if (url.pathname == '/submit') {
+	// 	if (request.method == 'GET') {
+    //         let p = url.searchParams.get('path');
+	// 		process(response, p);
+    //     }
+    //     else {
+    //         response.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+	// 		response.write(`This application does not support the ${request.method} method`);
+    //         response.end();
+    //     }
+	// }
 	else {
 		response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-		response.write(`<form method="GET" action="/submit">
-	    					<label for="path">Give a path</label>
-	    					<input name="path">
-	    					<br>
-	    					<input type="submit">
-	    					<input type="reset">
-	    				</form>`);
+        //use readfile żeby nie pierdolić się z tym htmlem
+		response.write(readHTML('response.html'));
 		response.end();
 	}
 }
 
 function process(response, p) {
+    console.log(p.split(''));
     fs.stat(p, (err, stats) => {
         if (err) {
             response.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
@@ -86,14 +121,15 @@ function process(response, p) {
     });
 }
 
-//var server = http.createServer(requestListener);
-//server.listen(8080);
-//debug("The server was started on port 8080");
-//debug("To stop the server, press 'CTRL + C'");
+var server = http.createServer(requestListener);
+server.listen(8080);
+console.log("The server was started on port 8080");
+console.log("To stop the server, press 'CTRL + C'");
 
-b1 = new Book('TYTUŁ','AuTorzy',1);
-console.log(b1.print())
-console.log(b1.borrow())
-console.log(b1.print())
-console.log(b1.return())
-console.log(b1.print())
+// u1 = new User('Szymon','Szymon',['TYTUŁ'])
+// b1 = new Book('TYTUŁ','AuTorzy',1);
+// console.log(b1.print());
+// console.log(b1.borrow(u1));
+// console.log(u1);
+// console.log(b1.return(u1));
+// console.log(u1);
