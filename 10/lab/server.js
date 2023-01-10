a43254d6db9df57229e7bbe695e6276b739672c7
szym-i <smilczarczyk@student.agh.edu.pl>
2023-01-10 21:21:44 +0100

    lab 10 late update

--- 10/lab/server.js
+++ 10/lab/server.js
@@ -2,42 +2,11 @@ const fs = require('fs');
 const http = require("http");
 var debug = require('debug')('http');
 
-class User{
-    constructor(name,surname,borrowed){
-        this.name = name;
-        this.surname = surname;
-        this.borrowed = borrowed;
-    }
-}
-
-class Book{
-    constructor(title,authors,n){
-        this.title = title;
-        this.authors = authors;
-        this.n = n;
-    }
-    print(){
-        return `${this.title} ${String(this.authors)} : ${this.n}`
-    }
-    borrow(user){
-        if (this.n > 0){
-            this.n-=1;
-            user.borrowed.push(this.name);
-            return 'The book has been borrowed'
-        }
-        else{
-            return "Not enough books"
-        }
-    }
-    return(user){
-        if (user.borrowed.includes(this.name)){
-            this.n+=1;
-            return 'The book has been returned'
-        }
-        else
-            return 'You cannot return this book'
-    }
-}
+const book1 = ["1984", ["G.Orwell"], "", "","/images/1984.jpg"];
+const book2 = ["Python", ["A","A"], "", "","/images/python.jpg"];
+const book3 = ["test",["test"],"test","test",,"/images/krzyk.jpg"];
+const book4 = ["Python", ["A","A"], "", "","/images/python.jpg"];
+let books = [book1,book2,book3,book4];
 
 function readHTML(arg){
     if (fs.existsSync(arg) === true){
@@ -45,6 +14,32 @@ function readHTML(arg){
             return(`${fs.readFileSync(arg, 'utf8')}`);
         }
     }
+}
+
+function readimage(arg){
+    if (fs.existsSync(arg) === true){
+        if (fs.lstatSync(arg).isFile()) {
+            return(`${fs.readFileSync(arg, 'utf8')}`);
+        }
+    }
+}
+
+function print_books(books){
+    let str='Avaliable books:<br>';
+    books.forEach(book =>{
+      if (book[2] === '' && book[3] == ''){
+        str+=`"${String(book[0])}" ${book[1]}<br>`;
+      }
+    });
+    return(str);
+  }
+
+function generateImages(books){
+    str = ''
+    books.forEach(book =>{
+        str+=`<img src="${book.at(-1)}">\n`
+    });
+    return str;
 }
 
 function handle(m){
@@ -63,22 +58,31 @@ function handle(m){
 function requestListener(request, response) {
 	var url = new URL(request.url, `http://${request.headers.host}`);
     if (request.method == 'POST') {
-        var body = ''
-        request.on('data', function(data) {
-             body += data;
-        })
-        request.on('end', function() {
-            body = body.slice(5);
-            debug('Received body: ' + body);
-            handle(body);
-            response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-            response.write(body);
-            response.end('\npost received');
-        })
+        if ( 1 === 1){
+            var body = ''
+            request.on('data', function(data) {
+                body += data;
+            })
+            request.on('end', function() {
+                body = body.slice(5);
+                debug('Received body: ' + body);
+                handle(body);
+                response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+                response.write(readHTML('response.html'));
+                //console.log(readimage('python.txt'));
+                response.write(`<img src="data:image/png;base64,${readHTML('images/python.txt')}" alt="Python" />`);
+                response.write(`<div>${body}</div>`);
+                response.write(`<div>${print_books(books)}</div>`);
+                response.end();
+            })
+
+        }
+
     }
 	else {
 		response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 		response.write(readHTML('response.html'));
+        response.write(`<div>${print_books(books)}</div>`);
 		response.end();
 	}
 }
